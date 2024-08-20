246a31c0093d7df230bb103ff61e111ebd4ddc82
Sergio Añez <160168084+SergioGygax@users.noreply.github.com>
2024-08-20 16:15:56 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -25,6 +25,53 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=True)
+    birth_year = Column(String(250), nullable=True)
+    planets = Column(String(250), ForeignKey('planets.id'))
+    starships = Column(String(250), ForeignKey('starships.id'))
+
+class FavCharacters(Base):
+    __tablename__ = 'favcharacters'
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship(Characters)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    gravity = Column(Integer, nullable=True)
+    rotation_period = Column(String(250), nullable=True)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship(Characters)
+
+class FavPlanets(Base):
+    __tablename__ = 'favplanets'
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet = relationship(Planets)
+    
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    passengers = Column(Integer, nullable=True)
+    speed = Column(String(250), nullable=True)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship(Characters)
+
     def to_dict(self):
         return {}
 
